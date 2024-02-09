7f85e3e9a0ede4b84a4919421af410e9b471af56
cosiee <andrewcos@yahoo.com>
2024-02-09 14:54:53 +0000

    navbar refined most styling removed from original. Bootstrap ranges and unances to be added

--- js/main.js
+++ js/main.js
@@ -18,9 +18,9 @@ gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'botto
 
 
     
-    $('#down').on('mouseenter', (e)=>{ gsap.to('#down', {y:10, duration:0.8, ease:'back.inOut(3)', overwrite:'auto'}); })
-    $('#down').on('mouseleave', (e)=>{ gsap.to('#down', {y:0, duration:0.5, ease:'power3.out', overwrite:'auto'}); })
-    $('#down').on('click', (e)=>{ gsap.to(window, {scrollTo:800, duration:4.5, ease:'power1.inOut'}); } ) // scrollTo requires the ScrollTo plugin (not to be confused w/ ScrollTrigger)
+    // $('#down').on('mouseenter', (e)=>{ gsap.to('#down', {y:10, duration:0.8, ease:'back.inOut(3)', overwrite:'auto'}); })
+    // $('#down').on('mouseleave', (e)=>{ gsap.to('#down', {y:0, duration:0.5, ease:'power3.out', overwrite:'auto'}); })
+    $('.scroll-arrow').on('click', (e)=>{ gsap.to(window, {scrollTo:600, duration:3, ease:'power3.inOut'}); } ) // scrollTo requires the ScrollTo plugin (not to be confused w/ ScrollTrigger)
 
 /*----------------------------------------------------*/
 /* Main Navigation 
