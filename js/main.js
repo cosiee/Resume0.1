55141e2d7b374488b47dfec6cacfe7114df99cd3
Cosiee <andrewcos@yahoo>
2024-03-21 15:54:27 +0000

    first commit from Linux machine

--- js/main.js
+++ js/main.js
@@ -74,36 +74,38 @@ gsap
     { scale: 2, x: -500, y: -690 },
     0
   )
+  // console.log("Hello world2!"+navbarSoft)
   .fromTo(
     ".thumbs#software",
     { scale: 2.5, x: -1500, y: -570 },
     { scale: 1, x: 320, y: -570 },
-    0 
+    0
   )
+  // console.log("Hello world3!"+navbarSoft)
   .fromTo(
     ".thumbs#photography",
     { scale: 2.5, x: 2400, y: -570 },
     { scale: 1, x: 620, y: -570 },
-    0 
+    0
   )
   .fromTo(
     ".thumbs#diy",
     { scale: 2.5, x: 2400, y: 700 },
     { scale: 1, x: 620, y: -270 },
-    0 
+    0
   )
   .fromTo(
     ".thumbs#videography",
     { scale: 2.5, x: -1500, y: 700 },
     { scale: 1, x: 320, y: -270 },
-    0  
+    0
   );
-
 
 // $('#down').on('mouseenter', (e)=>{ gsap.to('#down', {y:10, duration:0.8, ease:'back.inOut(3)', overwrite:'auto'}); })
 // $('#down').on('mouseleave', (e)=>{ gsap.to('#down', {y:0, duration:0.5, ease:'power3.out', overwrite:'auto'}); })
 $(".scroll-arrow").on("click", (e) => {
-  gsap.to(window, { scrollTo: 600, duration: 3, ease: "power3.inOut" });}); // scrollTo requires the ScrollTo plugin (not to be confused w/ ScrollTrigger)
+  gsap.to(window, { scrollTo: 600, duration: 3, ease: "power3.inOut" });
+}); // scrollTo requires the ScrollTo plugin (not to be confused w/ ScrollTrigger)
 
 /*----------------------------------------------------*/
 /* Main Navigation 
@@ -155,4 +157,123 @@ $(window).scroll(function () {
 // });
 
 // TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
-// 
+//
+
+// ==========================================================
+// index.html animate thumbnails and navbar items
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const thumbSoft = document.querySelector("#software");
+  const navbarSoft = document.querySelector(".dropdown-toggle");
+
+  const thumbPhoto = document.querySelector("#photography");
+  const navbarPhoto = document.querySelector("#photographyLink");
+
+  const thumbVid = document.querySelector("#videography");
+  const navbarVid = document.querySelector("#videographyLink");
+
+  const thumbDiy = document.querySelector("#diy");
+  const navbarDiy = document.querySelector("#diyLink");
+
+
+  thumbPhoto.addEventListener("mouseenter", function () {
+    console.log("Hello world!"+navbarPhoto);
+    navbarPhoto.style.scale = 1.2;
+    navbarPhoto.style.color = "#6e6f79";
+    thumbPhoto.style.scale = 1.004;
+  });
+
+  thumbPhoto.addEventListener("mouseleave", function () {
+    navbarPhoto.style.scale = 1;
+    navbarPhoto.style.color = "#162a43";
+    thumbPhoto.style.scale = 1;
+  });
+
+  navbarPhoto.addEventListener("mouseenter", function () {
+    navbarPhoto.style.scale = 1.02;
+    navbarPhoto.style.color = "#6e6f79";
+    thumbPhoto.style.scale = 1.004;
+  });
+
+  navbarPhoto.addEventListener("mouseleave", function () {
+    navbarPhoto.style.scale = 1;
+    navbarPhoto.style.color = "#162a43";
+    thumbPhoto.style.scale = 1;
+  });
+
+
+  thumbSoft.addEventListener("mouseenter", function () {
+    navbarSoft.style.scale = 1.2;
+    navbarSoft.style.color = "#6e6f79";
+    thumbSoft.style.scale = 1.004;
+  });
+
+  thumbSoft.addEventListener("mouseleave", function () {
+    navbarSoft.style.scale = 1;
+    navbarSoft.style.color = "#162a43";
+    thumbSoft.style.scale = 1;
+  });
+
+  navbarSoft.addEventListener("mouseenter", function () {
+    navbarSoft.style.scale = 1.02;
+    navbarSoft.style.color = "#6e6f79";
+    thumbSoft.style.scale = 1.004;
+  });
+
+  navbarSoft.addEventListener("mouseleave", function () {
+    navbarSoft.style.scale = 1;
+    navbarSoft.style.color = "#162a43";
+    thumbSoft.style.scale = 1;
+  });
+
+
+  thumbVid.addEventListener("mouseenter", function () {
+    navbarVid.style.scale = 1.2;
+    navbarVid.style.color = "#6e6f79";
+    thumbVid.style.scale = 1.004;
+  });
+
+  thumbVid.addEventListener("mouseleave", function () {
+    navbarVid.style.scale = 1;
+    navbarVid.style.color = "#162a43";
+    thumbVid.style.scale = 1;
+  });
+
+  navbarVid.addEventListener("mouseenter", function () {
+    navbarVid.style.scale = 1.02;
+    navbarVid.style.color = "#6e6f79";
+    thumbVid.style.scale = 1.004;
+  });
+
+  navbarVid.addEventListener("mouseleave", function () {
+    navbarVid.style.scale = 1;
+    navbarVid.style.color = "#162a43";
+    thumbVid.style.scale = 1;
+  });
+
+
+  thumbDiy.addEventListener("mouseenter", function () {
+    navbarDiy.style.scale = 1.2;
+    navbarDiy.style.color = "#6e6f79";
+    thumbDiy.style.scale = 1.004;
+  });
+
+  thumbDiy.addEventListener("mouseleave", function () {
+    navbarDiy.style.scale = 1;
+    navbarDiy.style.color = "#162a43";
+    thumbDiy.style.scale = 1;
+  });
+
+  navbarDiy.addEventListener("mouseenter", function () {
+    navbarDiy.style.scale = 1.02;
+    navbarDiy.style.color = "#6e6f79";
+    thumbDiy.style.scale = 1.004;
+  });
+
+  navbarDiy.addEventListener("mouseleave", function () {
+    navbarDiy.style.scale = 1;
+    navbarDiy.style.color = "#162a43";
+    thumbDiy.style.scale = 1;
+  });
+});
